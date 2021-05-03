12ec302868d53c6a9ba255e20a63a691b1032091
Henok Biadglign Ademtew <46082799+HenokB@users.noreply.github.com>
2021-05-03 06:43:07 -0400

    Delete Day 2 Operators.py

--- Day 2 Operators.py
+++
@@ -1,6 +0,0 @@
-def solve(meal_cost, tip_percent, tax_percent):
-    tip = (tip_percent/100)*meal_cost
-    tax = (tax_percent/100)*meal_cost
-    total_cost = meal_cost+tip+tax
-    
-    print(round(total_cost))
